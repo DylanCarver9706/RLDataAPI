c8b686ec293c92bfa8d64318f4b3d67ec5da7943
Dylan Carver <dylancarver14@gmail.com>
2023-01-21 09:48:28 -0600

    Make test seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 puts "Seeding Items..."
 
-day1 = DailyItem.create(name: "Octane", cost: 700, rarity: "Import Body", color: "Saffron", date: Time.new)
+item1 = DailyItem.create(name: "Octane", cost: 700, rarity: "Import Body", color: "Saffron", date: Time.new)
+item2 = DailyItem.create(name: "Hexed", cost: 2000, rarity: "Black Market Decal", color: null, date: Time.new)
+item3 = DailyItem.create(name: "Infinium", cost: 700, rarity: "Exotic Wheels", color: null, date: Time.new)
+item4 = DailyItem.create(name: "Storm Watch", cost: 2000, rarity: "Black Market Decal", color: null, date: Time.new)
+item5 = DailyItem.create(name: "Quimby", cost: 200, rarity: "Very Rare Wheels", color: null, date: Time.new)
+item6 = DailyItem.create(name: "Xenosplash", cost: 200, rarity: "Very Rare Rocket Boost", color: null, date: Time.new)
+item7 = DailyItem.create(name: "Tailwind: Schematized", cost: 600, rarity: "Import Wheels", color: "Black", date: Time.new)
+item8 = DailyItem.create(name: "Neoctane", cost: 100, rarity: "Rare Banner", color: null, date: Time.new)
