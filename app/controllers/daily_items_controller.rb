d9c4be5b5ae2c8903b328b8c270bfe535c9780d3
Dylan Carver <dylancarver14@gmail.com>
2023-01-21 10:04:17 -0600

    create basic routes and controller methods

--- app/controllers/daily_items_controller.rb
+++ app/controllers/daily_items_controller.rb
@@ -1,2 +1,27 @@
 class DailyItemsController < ApplicationController
+
+    def index
+        # render all instances
+        render json: DailyItem.all, status: :ok
+    end
+
+
+    def show
+        # render one instances
+        item = DailyItem.find(params[:date])
+        render json: item, status: 200
+    end
+
+    def create
+        new_item = DailyItem.create!(new_item_params)
+        # will render the activity based on the activity_id from the strong params instead of the signup json
+        # do this by adding belongs_to in serializer
+        render json: new_item, status: :created
+    end
+
+    private
+
+    def new_item_params
+        params.permit(:name, :cost, :rarity, :color, :date)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   resources :daily_items
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :campers, only: [:index, :show, :create]
 end
