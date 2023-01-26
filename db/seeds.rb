52c88730594314ebd278a00cb1b7a452bdb8f00a
Dylan Carver <dylancarver14@gmail.com>
2023-01-26 08:39:49 -0600

    add photos and update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 puts "Seeding Items..."
 
-item1 = DailyItem.create(name: "Octane", cost: 700, rarity: "Import Body", color: "Saffron", date: Time.new, image: )
-item2 = DailyItem.create(name: "Hexed", cost: 2000, rarity: "Black Market Decal", color: nil, date: Time.new)
-item3 = DailyItem.create(name: "Infinium", cost: 700, rarity: "Exotic Wheels", color: nil, date: Time.new)
-item4 = DailyItem.create(name: "Storm Watch", cost: 2000, rarity: "Black Market Decal", color: nil, date: Time.new)
-item5 = DailyItem.create(name: "Quimby", cost: 200, rarity: "Very Rare Wheels", color: nil, date: Time.new)
-item6 = DailyItem.create(name: "Xenosplash", cost: 200, rarity: "Very Rare Rocket Boost", color: nil, date: Time.new)
-item7 = DailyItem.create(name: "Tailwind: Schematized", cost: 600, rarity: "Import Wheels", color: "Black", date: Time.new)
-item8 = DailyItem.create(name: "Neoctane", cost: 100, rarity: "Rare Banner", color: nil, date: Time.new)
+item1 = DailyItem.create(name: "Octane", cost: 700, rarity: "Import Body", color: "Saffron", date: Time.new, image: "RLDataApi/db/images/RL1.PNG")
+item2 = DailyItem.create(name: "Hexed", cost: 2000, rarity: "Black Market Decal", color: nil, date: Time.new, image: "RLDataApi/db/images/RL2.PNG")
+item3 = DailyItem.create(name: "Infinium", cost: 700, rarity: "Exotic Wheels", color: nil, date: Time.new, image: "RLDataApi/db/images/RL3.PNG")
+item4 = DailyItem.create(name: "Storm Watch", cost: 2000, rarity: "Black Market Decal", color: nil, date: Time.new, image: "RLDataApi/db/images/RL1.PNG")
+item5 = DailyItem.create(name: "Quimby", cost: 200, rarity: "Very Rare Wheels", color: nil, date: Time.new, image: "RLDataApi/db/images/RL2.PNG")
+item6 = DailyItem.create(name: "Xenosplash", cost: 200, rarity: "Very Rare Rocket Boost", color: nil, date: Time.new, image: "RLDataApi/db/images/RL3.PNG")
+item7 = DailyItem.create(name: "Tailwind: Schematized", cost: 600, rarity: "Import Wheels", color: "Black", date: Time.new, image: "RLDataApi/db/images/RL1.PNG")
+item8 = DailyItem.create(name: "Neoctane", cost: 100, rarity: "Rare Banner", color: nil, date: Time.new, image: "RLDataApi/db/images/RL2.PNG")
