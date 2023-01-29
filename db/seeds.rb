f4db5fe894598ba651dbdf7575f7ca522ae79ffb
Dylan Carver <dylancarver14@gmail.com>
2023-01-29 00:23:40 -0600

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 puts "Seeding Items..."
 
-item1 = DailyItem.create(name: "Octane", cost: 700, rarity: "Import Body", color: "Saffron", date: Time.new, image: "https://i.imgur.com/O3vNJRN.png")
-item2 = DailyItem.create(name: "Hexed", cost: 2000, rarity: "Black Market Decal", color: false, date: Time.new, image: "https://i.imgur.com/DgVyPA5.png")
-item3 = DailyItem.create(name: "Infinium", cost: 700, rarity: "Exotic Wheels", color: false, date: Time.new, image: "https://i.imgur.com/7dwrPiy.png")
-item4 = DailyItem.create(name: "Storm Watch", cost: 2000, rarity: "Black Market Decal", color: false, date: Time.new, image: "https://i.imgur.com/O3vNJRN.png")
-item5 = DailyItem.create(name: "Quimby", cost: 200, rarity: "Very Rare Wheels", color: false, date: Time.new, image: "https://i.imgur.com/DgVyPA5.png")
-item6 = DailyItem.create(name: "Xenosplash", cost: 200, rarity: "Very Rare Rocket Boost", color: false, date: Time.new, image: "https://i.imgur.com/7dwrPiy.png")
-item7 = DailyItem.create(name: "Tailwind: Schematized", cost: 600, rarity: "Import Wheels", color: "Black", date: Time.new, image: "https://i.imgur.com/O3vNJRN.png")
-item8 = DailyItem.create(name: "Neoctane", cost: 100, rarity: "Rare Banner", color: false, date: Time.new, image: "https://i.imgur.com/DgVyPA5.png")
+DailyItem.create(name: "Octane", cost: 700, rarity: "Import Body", color: "Saffron", date: Time.new, image: "https://i.imgur.com/O3vNJRN.png")
+DailyItem.create(name: "Hexed", cost: 2000, rarity: "Black Market Decal", color: false, date: Time.new, image: "https://i.imgur.com/DgVyPA5.png")
+DailyItem.create(name: "Infinium", cost: 700, rarity: "Exotic Wheels", color: false, date: Time.new, image: "https://i.imgur.com/7dwrPiy.png")
+DailyItem.create(name: "Storm Watch", cost: 2000, rarity: "Black Market Decal", color: false, date: Time.new, image: "https://i.imgur.com/O3vNJRN.png")
+DailyItem.create(name: "Quimby", cost: 200, rarity: "Very Rare Wheels", color: false, date: Time.new, image: "https://i.imgur.com/DgVyPA5.png")
+DailyItem.create(name: "Xenosplash", cost: 200, rarity: "Very Rare Rocket Boost", color: false, date: Time.new, image: "https://i.imgur.com/7dwrPiy.png")
+DailyItem.create(name: "Tailwind: Schematized", cost: 600, rarity: "Import Wheels", color: "Black", date: Time.new, image: "https://i.imgur.com/O3vNJRN.png")
+DailyItem.create(name: "Neoctane", cost: 100, rarity: "Rare Banner", color: false, date: Time.new, image: "https://i.imgur.com/DgVyPA5.png")
