6ce5693c75e034887a97fc80e28e9e4da47106e4
Dylan Carver <dylancarver14@gmail.com>
2023-03-11 08:57:53 -0600

    add welcome route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   resources :items, only: [:index, :show, :create, :update, :destroy]
   resources :item_shop_dates
   resources :daily_items, only: [:index, :show, :create, :update, :destroy]
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root "rails/welcome#index"
 end
