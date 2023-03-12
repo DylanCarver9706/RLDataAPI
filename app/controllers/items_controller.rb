81c8b990698d66c6a06be0b77ec656735bc58494
Dylan Carver <dylancarver14@gmail.com>
2023-03-11 23:32:17 -0600

    fix create method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,7 +12,7 @@ class ItemsController < ApplicationController
     end
 
     def create
-        new_item = Item.create!(new_item_params)
+        new_item = Item.create!(item_params.except(:id))
         # will render the activity based on the activity_id from the strong params instead of the signup json
         # do this by adding belongs_to in serializer
         render json: new_item, status: :created
@@ -36,7 +36,7 @@ class ItemsController < ApplicationController
     private
 
     def new_item_params
-        params.permit(:id, :image, :valid_status, :name, :image_uri, :rarity, :item_type, :image_location, :color)
+        params.permit(:id, :image, :valid_status, :name, :image_uri, :rarity, :item_type, :color)
     end
 
     def item_params
