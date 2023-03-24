07843d26ae6cecf6efae5c86c2c45749984c254b
Dylan Carver <dylancarver14@gmail.com>
2023-03-23 20:26:51 -0500

    fix serializers

--- app/serializers/crate_serializer.rb
+++ app/serializers/crate_serializer.rb
@@ -1,3 +1,3 @@
 class CrateSerializer < ActiveModel::Serializer
-  attributes :id, :"valid_status,", :"image_uri,", :"name,", :"rarity,", :"item_type,", :"color,", :"image_location,", :image
+  attributes :id, :valid_status, :image_uri, :name, :rarity, :item_type, :color, :image_location, :image
 end
--- app/serializers/paint_finish_serializer.rb
+++ app/serializers/paint_finish_serializer.rb
@@ -1,3 +1,3 @@
 class PaintFinishSerializer < ActiveModel::Serializer
-  attributes :id, :"valid_status,", :"image_uri,", :"name,", :"rarity,", :"item_type,", :"color,", :"image_location,", :image
+  attributes :id, :valid_status, :image_uri, :name, :rarity, :item_type, :color, :image_location, :image
 end
