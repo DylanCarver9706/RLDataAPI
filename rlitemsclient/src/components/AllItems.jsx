6b044c2062668c3506c2179744d0ec7d31d48e95
Dylan Carver <dylancarver14@gmail.com>
2023-03-30 12:41:43 -0500

    update code to display all items from all endpoint

--- rlitemsclient/src/components/AllItems.jsx
+++ rlitemsclient/src/components/AllItems.jsx
@@ -13,10 +13,35 @@ const ItemList = () => {
 
     const itemsPerPage = 24;
 
-    const fetchData = async () => {
-        const response = await fetch('http://localhost:3000/items');
+    const fetchItems = async (endpoint) => {
+        const response = await fetch(`http://localhost:3000/${endpoint}`);
         const data = await response.json();
-        setItems(data);
+        return data;
+    };
+
+    const fetchData = async () => {
+        const endpoints = [
+            'items',
+            'antennas',
+            'avatar_borders',
+            'avatars',
+            'banners',
+            'boosts',
+            'cars',
+            'crates',
+            'decals',
+            'engine_sounds',
+            'goal_exposions',
+            'paint_finishes',
+            'player_anthems',
+            'stickers',
+            'toppers',
+            'trails',
+            'wheels'
+        ];
+        const itemData = await Promise.all(endpoints.map(fetchItems));
+        const flattenedData = itemData.flat();
+        setItems(flattenedData);
     };
 
     useEffect(() => {
@@ -69,12 +94,12 @@ const ItemList = () => {
                     <Card
                         key={item.id}
                         hoverable
-                        style={{ width: '100%' }}
+                        style={{ width: '100%', maxWidth: '300px' }}
                         cover={
                             <img
                                 alt={item.name}
                                 src={`${item.image}`}
-                                style={{ height: 200, objectFit: 'contain' }}
+                                style={{ height: 'auto', width: '100%', objectFit: 'contain' }}
                             />
                         }
                     >
@@ -92,3 +117,100 @@ const ItemList = () => {
 };
 
 export default ItemList;
+
+
+
+// import React, { useState, useEffect } from 'react';
+// import { Card, Input } from 'antd';
+
+// const { Meta } = Card;
+// const { Search } = Input;
+
+// const ItemList = () => {
+//     const [items, setItems] = useState([]);
+//     const [displayedItems, setDisplayedItems] = useState([]);
+//     const [currentPage, setCurrentPage] = useState(1);
+//     const [hasMoreItems, setHasMoreItems] = useState(true);
+//     const [searchTerm, setSearchTerm] = useState('');
+
+//     const itemsPerPage = 24;
+
+//     const fetchData = async () => {
+//         const response = await fetch('http://localhost:3000/items');
+//         const data = await response.json();
+//         setItems(data);
+//     };
+
+//     useEffect(() => {
+//         fetchData();
+//     }, []);
+
+//     useEffect(() => {
+//         setCurrentPage(1);
+//     }, [searchTerm]);
+
+//     useEffect(() => {
+//         const filteredItems = items.filter((item) =>
+//             item.name.toLowerCase().includes(searchTerm.toLowerCase())
+//         );
+//         setDisplayedItems(filteredItems.slice(0, currentPage * itemsPerPage));
+//         setHasMoreItems(filteredItems.length > currentPage * itemsPerPage);
+//     }, [items, searchTerm, currentPage]);
+
+//     const loadMore = () => {
+//         setDisplayedItems([
+//             ...displayedItems,
+//             ...items.slice(
+//                 currentPage * itemsPerPage,
+//                 (currentPage + 1) * itemsPerPage
+//             ),
+//         ]);
+//         setCurrentPage(currentPage + 1);
+//     };
+
+//     const handleSearch = (event) => {
+//         setSearchTerm(event.target.value);
+//     };
+
+//     return (
+//         <>
+//             <Search
+//                 placeholder="Search by item name"
+//                 onChange={handleSearch}
+//                 style={{ width: 300, marginBottom: 20 }}
+//             />
+//             <div
+//                 style={{
+//                     display: 'grid',
+//                     gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+//                     gridGap: 20,
+//                     justifyContent: 'center',
+//                 }}
+//             >
+//                 {displayedItems.map((item) => (
+//                     <Card
+//                         key={item.id}
+//                         hoverable
+//                         style={{ width: '100%' }}
+//                         cover={
+//                             <img
+//                                 alt={item.name}
+//                                 src={`${item.image}`}
+//                                 style={{ height: 200, objectFit: 'contain' }}
+//                             />
+//                         }
+//                     >
+//                         <Meta title={item.name} description={item.item_type} />
+//                     </Card>
+//                 ))}
+//             </div>
+//             {hasMoreItems && (
+//                 <button onClick={loadMore} style={{ marginTop: 20 }}>
+//                     Load more items
+//                 </button>
+//             )}
+//         </>
+//     );
+// };
+
+// export default ItemList;
