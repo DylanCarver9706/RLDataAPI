8001ec1627e2304834e9de392a855c98a4bb9fc5
Dylan Carver <dylancarver14@gmail.com>
2023-03-31 09:59:54 -0500

    install material ui

--- rlitemsclient/src/App.js
+++ rlitemsclient/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AllItems from './components/AllItems';
 
 function App() {
   return (
     <div>
       <h1>RocketLeagueItemsAPI.com</h1>
-      <AllItems />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AllItems />} />
+          <Route path="/wheels" element={<Wheels />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
